80006cd012bee16c3fe93a79ffc7f7ce19b4b0fb
hajam09 <hajam09@yahoo.com>
2019-11-24 18:06:25 +0000

    changed settingsfile

--- auctionapp/settings.py
+++ auctionapp/settings.py
@@ -134,15 +134,11 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'staticfiles'),
 )
 
-TEMPLATE_DIRS = [ os.path.join(BASE_DIR, 'templates'), ]
-
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
                     os.path.join(BASE_DIR, 'static'),
 )
-
-STATIC_ROOT = os.path.join(BASE_DIR, '../static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
